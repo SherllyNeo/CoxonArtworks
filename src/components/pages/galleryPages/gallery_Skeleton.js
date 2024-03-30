c58d2e5f370d991dfc79157bbd210becf2372556
sherllyneo <jacobp.willo@gmail.com>
2024-03-30 21:51:41 +0000

    changed about

--- src/components/pages/galleryPages/gallery_Skeleton.js
+++ src/components/pages/galleryPages/gallery_Skeleton.js
@@ -4,10 +4,10 @@ import '../../../App.js';
 function Gallery_Skeleton() {
     return (
         <>
-        <h1 style={{fontSize=50}}> Skeleton Artwork </h1>
+        <h1 style={{fontSize:50}}> Skeleton Artwork </h1>
         <img src='https://raw.githubusercontent.com/SherllyNeo/CoxonArtworks/main/public/images/skeleton.jpeg' alt="image not found" width="500" height="700" class="center"/>
         <center>
-        <p style={{fontSize=30}}> This art work is about blah blah </p>
+        <p style={{fontSize:30}}> This art work is about blah blah </p>
         </center>
         </>
     )
