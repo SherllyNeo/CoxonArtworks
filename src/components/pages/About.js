2248118c60b94a5f130685e84f79a8ab1887d2a8
sherllyneo <jacobp.willo@gmail.com>
2024-03-30 21:49:47 +0000

    changed about

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,18 +6,25 @@ import './About.css';
 function About() {
     return (
         <div className='about'>
-        <div classNAme='aboutContainer'>
-            <h1 style={{fontSize: 100, padding: 50}}> Elizabeth Coxon </h1>
+            <div classNAme='aboutContainer'>
+                    <h1 style={{fontSize: 100, padding: 50}}> Elizabeth Coxon </h1>
 
-            <div align="right" style={{marginRight: 450 }}>
-            <img src="images/headshot.jpg" alt="headshot" height="200" width="200" />
-            </div>
 
-            <div align="left">
-            <p style={{fontSize: 50, padding: 50}}> I am an amitious artist looking to make an impact in the industry and explore new and exciting mediums of expression </p>
-            <p style={{fontSize: 50, padding: 50}}> I love to make stuff </p>
+                <div class="float-container">
+
+                <div class="float-child">
+                    <div align="right" style={{marginRight: 450 }}>
+                    <img src="images/headshot.jpg" alt="headshot" height="200" width="200" />
+                </div>
+                </div>
+
+                <div class="float-child">
+                    <div align="left">
+                    <p style={{fontSize: 50, padding: 50}}> I am an amitious artist looking to make an impact in the industry and explore new and exciting mediums of expression </p>
+                    <p style={{fontSize: 50, padding: 50}}> I love to make stuff </p>
+                    </div>
+                </div>
             </div>
-        </div>
         </div>
     )
 }
--- src/components/pages/galleryPages/gallery_Skeleton.js
+++ src/components/pages/galleryPages/gallery_Skeleton.js
@@ -4,9 +4,11 @@ import '../../../App.js';
 function Gallery_Skeleton() {
     return (
         <>
-        <h1> Skeleton Artwork </h1>
+        <h1 style={{fontSize=50}}> Skeleton Artwork </h1>
         <img src='https://raw.githubusercontent.com/SherllyNeo/CoxonArtworks/main/public/images/skeleton.jpeg' alt="image not found" width="500" height="700" class="center"/>
-        <p> This art work is about blah blah </p>
+        <center>
+        <p style={{fontSize=30}}> This art work is about blah blah </p>
+        </center>
         </>
     )
 
