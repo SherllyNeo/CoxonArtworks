a240f734066a702d48ad176fc80fd9f67c4ba4ed
sherllyneo <jacobp.willo@gmail.com>
2024-03-30 20:18:13 +0000

    following type path tutorial

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {HashRouter as Router, Routes, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Navbar from './components/Navbar.js';
 import Home from './components/pages/Home.js'
 import About from './components/pages/About.js'
@@ -13,10 +13,10 @@ import './App.css';
 function App() {
   return (
       <>
-      <Router>
+      <Router basename="/CoxonArtworks">
         <Navbar/>
         <Routes>
-            <Route path='/' exact element={<Home />} /> 
+            <Route path='/CoxonArtworks' exact element={<Home />} /> 
             <Route path='/about' exact element={<About />} /> 
             <Route path='/contact' exact element={<Contact />} /> 
             <Route path='/gallery' exact element={<Gallery />} /> 
