bc450c573be24c6ee76560f2f080262c8de68e22
sherllyneo <jacobp.willo@gmail.com>
2024-03-31 15:02:05 +0100

    finally got a decent looking about me

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,34 +1,28 @@
 import React from 'react';
-import '../../App.css';
 import './About.css';
 
 
 function About() {
     return (
-        <div className='about'>
-            <div classNAme='aboutContainer'>
-                    <h1 style={{fontSize: 100, padding: 50}}> Elizabeth Coxon </h1>
-
-
-                <div class="float-container">
-
-                <div class="float-child">
-                    <div align="right" style={{marginRight: 450 }}>
-                    <img src="images/aboutMe.jpeg" alt="headshot" height="600" width="600" />
-                </div>
-                </div>
-
-                <div class="float-child">
-                    <div align="left">
-                    <p style={{fontSize: 25, padding: 10}}> The upcoming artist Elizabeth Coxon, who was born in England in 2002, is a promising young artist who explores a variety of mediums and techniques in her work. </p>
-                    <p style={{fontSize: 25, padding: 10}}>Her work includes printmaking, painting, and drawing, among other things. The artworks of Coxon are a variety of unique collections that she has developed over the course of her time in art. </p>
-                    <p style={{fontSize: 25, padding: 10}}>The city of Leicester is where Elizabeth is now residing, and she is working towards earning a bachelor's degree in fine art. She takes an active role in the study and curation of her work, which is continually influenced by the natural world and the human body. </p>
-                     <p style={{fontSize: 25, padding: 10}}>The usual viewpoints in the industry are challenged by her art as she investigates the use of colour and mediums in singular ways. She is always looking for new ways to combine traditional techniques with modern elements in her artwork, and she actively seeks out new ways to improve and progress her talents.</p>
-                    </div>
-                </div>
-            </div>
+        <>
+        <div class="column-2">
+            <h1>About Lizzy</h1>
+<p>            The upcoming artist Elizabeth Coxon, who was born in England in 2002, is a promising young artist who explores a variety of mediums and techniques in her work. </p>
+<p>            Her work includes printmaking, painting, drawing and other techniques. </p>
+<p>            The artworks of Coxon are a varied collection of works that she has developed over the course of her time in the art industry. </p>
+<p>            The city of Leicester is where Elizabeth is now residing, and she is working towards earning a bachelor's degree in fine art. </p>
+<p>            She takes an active role in the study and curation of her work, which is continually influenced by the natural world and the human body. </p>
+<p>            The usual ideas of the industry are challenged by her art, via investigating novel uses of colour, expression and ideas. </p>
+<p>            She is always looking for new ways to combine traditional techniques with modern elements in her artwork, and she actively seeks out new ways expand her creative endevours. </p>
         </div>
+        <div class="column-2 large-profile">
+        <img 
+          alt="Elizbeth Coxon" 
+          src='https://raw.githubusercontent.com/SherllyNeo/CoxonArtworks/main/public/images/aboutMe.jpeg'
+          style={{ height: "200%", paddingRight: "10px" }} 
+        />
         </div>
+        </>
     )
 }
 
