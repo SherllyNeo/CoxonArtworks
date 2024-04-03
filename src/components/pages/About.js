aeafd67cdd86e87ed64af5472767d26643d36ead
sherllyneo <jacobp.willo@gmail.com>
2024-04-03 06:23:34 +0100

    fixed about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,8 +5,17 @@ import './About.css';
 function About() {
     return (
         <div class="container">
+    <div class="column">
+        <h1>About Lizzy</h1>
+    </div>
+    <div class="column large-profile">
+    <img 
+      alt="Elizbeth Coxon" 
+      src='https://raw.githubusercontent.com/SherllyNeo/CoxonArtworks/main/public/images/aboutMe.jpeg'
+      style={{ height: "100%",width: "100%", maxWidth: "500px",   display: "block", marginLeft: "auto", marginRight: "auto" }} 
+    />
+    </div>
         <div class="column">
-            <h1>About Lizzy</h1>
 <p>            The upcoming artist Elizabeth Coxon, who was born in England in 2002, is a promising young artist who explores a variety of mediums and techniques in her work. </p>
 <p>            Her work includes printmaking, painting, drawing and other techniques. </p>
 <p>            The artworks of Coxon are a varied collection of works that she has developed over the course of her time in the art industry. </p>
@@ -14,13 +23,6 @@ function About() {
 <p>            She takes an active role in the study and curation of her work, which is continually influenced by the natural world and the human body. </p>
 <p>            The usual ideas of the industry are challenged by her art, via investigating novel uses of colour, expression and ideas. </p>
 <p>            She is always looking for new ways to combine traditional techniques with modern elements in her artwork, and she actively seeks out new ways expand her creative endevours. </p>
-        </div>
-        <div class="column large-profile">
-        <img 
-          alt="Elizbeth Coxon" 
-          src='https://raw.githubusercontent.com/SherllyNeo/CoxonArtworks/main/public/images/aboutMe.jpeg'
-          style={{ height: "100%",width: "100%", paddingRight: "20px" }} 
-        />
         </div>
         </div>
     )
